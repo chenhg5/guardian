22a6b0ffa0eb53b34d912fb0259ed7a6a9c80057
cg33 <chg80333@163.com>
2018-09-26 17:40:32 +0800

    add request method

--- request.go
+++ request.go
@@ -1,27 +1,123 @@
 package guardian
 
-import "net/http"
+import (
+	"net/http"
+	"bytes"
+	"github.com/gin-gonic/gin/json"
+	neturl "net/url"
+	"strings"
+)
 
-type RequestFunc func(url string, params interface{}, header map[string]string) http.Response
+type RequestFunc func(url string, params interface{}, headers map[string]string) *http.Response
 
-func Json(url string, params interface{}, header map[string]string) http.Response {
-	// TODO: 发送json post请求
-	return http.Response{}
+func Json(url string, params interface{}, headers map[string]string) *http.Response {
+	var (
+		postDataByte []byte
+		err          error
+	)
+
+	if postDataByte, err = json.Marshal(params); err != nil {
+		return &http.Response{}
+	}
+
+	client := http.DefaultClient
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(postDataByte))
+	if err != nil {
+		return &http.Response{}
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	for key, header := range headers {
+		req.Header.Set(key, header)
+	}
+
+	res, err := client.Do(req)
+
+	if err != nil {
+		return &http.Response{}
+	}
+
+	return res
 }
 
-func Get(url string, params interface{}, header map[string]string) http.Response {
-	// TODO: 发送get请求
-	return http.Response{}
+func Get(url string, params interface{}, headers map[string]string) *http.Response {
+	var count = 0
+	data := params.(map[string]string)
+	for k, v := range data {
+		if count == 0 {
+			url += "?" + k + "=" + v
+		} else {
+			url += "&" + k + "=" + v
+		}
+		count++
+	}
+
+	client := http.DefaultClient
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return &http.Response{}
+	}
+
+	for key, header := range headers {
+		req.Header.Set(key, header)
+	}
+
+	res, _ := client.Do(req)
+
+	if err != nil {
+		return &http.Response{}
+	}
+	return res
 }
 
-func FormGet(url string, params interface{}, header map[string]string) http.Response {
-	// TODO: 发送form get请求
-	return http.Response{}
+func FormGet(url string, params interface{}, headers map[string]string) *http.Response {
+	data := make(neturl.Values)
+	formData := params.(map[string]string)
+
+	for k, v := range formData {
+		data[k] = []string{v}
+	}
+
+	client := http.DefaultClient
+	req, err := http.NewRequest("GET", url, strings.NewReader(data.Encode()))
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	for key, header := range headers {
+		req.Header.Set(key, header)
+	}
+
+	res, _ := client.Do(req)
+
+	if err != nil {
+		return &http.Response{}
+	}
+	return res
 }
 
-func FormPost(url string, params interface{}, header map[string]string) http.Response {
-	// TODO: 发送form post请求
-	return http.Response{}
+func FormPost(url string, params interface{}, headers map[string]string) *http.Response {
+	data := make(neturl.Values)
+	formData := params.(map[string]string)
+
+	for k, v := range formData {
+		data[k] = []string{v}
+	}
+
+	client := http.DefaultClient
+	req, err := http.NewRequest("POST", url, strings.NewReader(data.Encode()))
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	for key, header := range headers {
+		req.Header.Set(key, header)
+	}
+
+	res, _ := client.Do(req)
+
+	if err != nil {
+		return &http.Response{}
+	}
+	return res
 }
 
 func GetRequester(key string) RequestFunc {
