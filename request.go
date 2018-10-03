ae908f659f8e59ccbba994a7bf59fac41a62686f
cg33 <chg80333@163.com>
2018-10-03 10:20:47 +0800

    add more request method

--- request.go
+++ request.go
@@ -46,6 +46,30 @@ func Json(url string, params interface{}, headers map[string]string) *http.Respo
 }
 
 func Get(url string, params interface{}, headers map[string]string) *http.Response {
+	return Request("GET", url, params, headers)
+}
+
+func Post(url string, params interface{}, headers map[string]string) *http.Response {
+	return Request("POST", url, params, headers)
+}
+
+func Delete(url string, params interface{}, headers map[string]string) *http.Response {
+	return Request("DELETE", url, params, headers)
+}
+
+func Head(url string, params interface{}, headers map[string]string) *http.Response {
+	return Request("HEAD", url, params, headers)
+}
+
+func Put(url string, params interface{}, headers map[string]string) *http.Response {
+	return Request("PUT", url, params, headers)
+}
+
+func Options(url string, params interface{}, headers map[string]string) *http.Response {
+	return Request("OPTIONS", url, params, headers)
+}
+
+func Request(method string, url string, params interface{}, headers map[string]string) *http.Response {
 	var count = 0
 	data := params.(map[string]interface{})
 	var value string
@@ -62,7 +86,7 @@ func Get(url string, params interface{}, headers map[string]string) *http.Respon
 
 	client := http.DefaultClient
 
-	req, err := http.NewRequest("GET", url, nil)
+	req, err := http.NewRequest(method, url, nil)
 	if err != nil {
 		panic(err)
 	}
@@ -81,33 +105,14 @@ func Get(url string, params interface{}, headers map[string]string) *http.Respon
 }
 
 func FormGet(url string, params interface{}, headers map[string]string) *http.Response {
-	data := make(neturl.Values)
-	formData := params.(map[string]interface{})
-	var value string
-	for k, v := range formData {
-		value = v.(string)
-		v = GlobalVars.Replace(value)
-		data[k] = []string{value}
-	}
-
-	client := http.DefaultClient
-	req, err := http.NewRequest("GET", url, strings.NewReader(data.Encode()))
-	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-
-	for key, header := range headers {
-		header = GlobalVars.Replace(header)
-		req.Header.Set(key, header)
-	}
-
-	res, _ := client.Do(req)
-
-	if err != nil {
-		panic(err)
-	}
-	return res
+	return Form("GET", url, params, headers)
 }
 
 func FormPost(url string, params interface{}, headers map[string]string) *http.Response {
+	return Form("POST", url, params, headers)
+}
+
+func Form(method string, url string, params interface{}, headers map[string]string) *http.Response {
 	data := make(neturl.Values)
 	formData := params.(map[string]interface{})
 
@@ -119,7 +124,7 @@ func FormPost(url string, params interface{}, headers map[string]string) *http.R
 	}
 
 	client := http.DefaultClient
-	req, err := http.NewRequest("POST", url, strings.NewReader(data.Encode()))
+	req, err := http.NewRequest(method, url, strings.NewReader(data.Encode()))
 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 
 	for key, header := range headers {
@@ -136,11 +141,19 @@ func FormPost(url string, params interface{}, headers map[string]string) *http.R
 }
 
 func GetRequester(key string) RequestFunc {
-	switch key {
+	switch strings.ToLower(key) {
 	case "json":
 		return Json
 	case "get":
 		return Get
+	case "post":
+		return Post
+	case "delete":
+		return Delete
+	case "put":
+		return Put
+	case "options":
+		return Options
 	case "formget":
 		return FormGet
 	case "formpost":
